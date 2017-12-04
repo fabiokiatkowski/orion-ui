c0a2fee36e99fc70c135f24ecb2a844f69889c69
Paulo Ricardo <paulor1809@gmail.com>
2017-12-04 19:30:50 -0200

    Add eslint

---
+++ devServer.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+
+const CONFIG_FILE = process.env.WEBPACK_CONFIG || 'webpack.config.dev';
+const config = require(`./${CONFIG_FILE}`); // eslint-disable-line
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  // It suppress error shown in console, so it has to be set to false.
+  quiet: false,
+  // It suppress everything except error, so it has to be set to false as well
+  // to see success build.
+  noInfo: false,
+  stats: {
+    // Config for minimal console.log mess.
+    assets: false,
+    colors: true,
+    version: false,
+    hash: false,
+    timings: false,
+    chunks: false,
+    chunkModules: false
+  }
+}).listen(3000, 'localhost', (err) => {
+  if (err) {
+    console.log(err); // eslint-disable-line
+  }
+  console.log('Listening at localhost:3000'); // eslint-disable-line
+});
--- src/js/components/StageComponent.js
+++ src/js/components/StageComponent.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function StageComponent(props) {
   const listStage = props.stages.map((stage) => {
-       return <li key={stage.stageId.toString()} className="list-group-item">{stage.stageName}</li>
-   });
-   return (
-       <ul className="list-group">{listStage}</ul>
-   );
+    return <li key={stage.stageId.toString()} className="list-group-item">{stage.stageName}</li>
+  });
+  return (
+    <ul className="list-group">{listStage}</ul>
+  );
 }
