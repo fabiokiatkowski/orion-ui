7c3128172c60a76779b3ffb744b9b0b2449d7370
Fabio Kiatkowski <xolla@xolla.com.br>
2017-12-05 18:10:27 -0200

    Final enviroment configuration

--- src/js/store/configureStore.js
+++ src/js/store/configureStore.js
@@ -1,6 +1,22 @@
-import configureStoreDev from './configureStore.dev';
-import configureStoreProd from './configureStore.prod';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createLogger} from 'redux-logger'; //eslint-disable-line
+import thunk from 'redux-thunk';
+import rootReducer from '../redux/modules/reducer';
 
-console.log(process.env.NODE_ENV);
-console.log(configureStoreDev);
-export default (process.env.NODE_ENV === 'production') ? configureStoreProd : configureStoreDev;
+const DEV = process.env.NODE_ENV === 'development';
+
+const logger = createLogger({ collapse: true });
+
+const finalStoreProd = compose(applyMiddleware(thunk))(createStore);
+const finalStoreDev = compose(applyMiddleware(thunk, logger))(createStore);
+
+module.exports = function configureStore(initialState) {
+  const store = (DEV) ? finalStoreDev(rootReducer, initialState) :
+    finalStoreProd(rootReducer, initialState);
+
+  if (DEV && module.hot) {
+    module.hot.accept('../redux/modules/reducer', () =>
+      store.replaceReducer(require('../redux/modules/reducer').default)); //eslint-disable-line
+  }
+  return store;
+};
