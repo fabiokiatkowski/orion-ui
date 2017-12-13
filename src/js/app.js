49d2ba6952397742374552d69f126401db921d6e
Fabio Kiatkowski <xolla@xolla.com.br>
2017-12-13 15:17:24 -0200

    Fix import

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import PanelRepresentantes from './containers/PanelRepresentantes';
 import createStore from './store/configureStore';
-import FlipCard from './components/flipcard-exp';
 import Page from './components/Page';
 import '../css/style.scss';
 
@@ -12,7 +11,6 @@ const store = createStore();
 const App = () => {
   return (
     <Page title="Painel Representantes">
-      <FlipCard />
       <PanelRepresentantes />
     </Page>
   );
