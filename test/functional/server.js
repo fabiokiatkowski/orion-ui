1f48d95ee61eb404aaba591b80d20f75a5d51ab3
paulorsouza <paulor1809@gmail.com>
2018-01-08 15:19:07 -0200

    Add server

--- test/functional/server.js
+++ test/functional/server.js
@@ -1,31 +1,8 @@
 import { createServer } from 'service-mocker/server'; // eslint-disable-line
-import { clientesInfo, singleCliente } from '../js/fixtures/clientes';
-import productsMock from '../js/fixtures/products';
-import condicoesPagamento from '../js/fixtures/cond-pgto';
-import tiposVenda from '../js/fixtures/tipos-venda';
+import representantes from '../fixtures/representantes';
 
 const { router } = createServer();
 
-router.get('/static/britania/produtos/:cnpj', (req, res) => {
-  res.send(productsMock);
-});
-
-router.get('/static/britania/clientes/1-2-17', (req, res) => {
-  res.send(singleCliente);
-});
-
-router.get('/static/britania/clientes', (req, res) => {
-  res.send(clientesInfo);
-});
-
-router.get('/static/britania/tiposVenda', (req, res) => {
-  res.send(tiposVenda);
-});
-
-router.get('/static/britania/condpgto', (req, res) => {
-  res.send(condicoesPagamento);
-});
-
-router.post('/static/britania/pedidos/', (req, res) => {
-  res.status(204).send();
+router.get('http://192.168.1.120:8080/orion-server/api/representantes/mysql/totalPedidosRecebidos', (req, res) => {
+  res.send(representantes);
 });
