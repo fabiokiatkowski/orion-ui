60c44bc3cc27a88b206ac12a63b3657abead77d2
paulorsouza <paulor1809@gmail.com>
2018-01-12 16:19:18 -0200

    Trick to deserialize filter on grid

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -1,9 +1,18 @@
+import MultiCheckFilter from '../components/MultiCheckFilter';
+
 const ColumnPersist = {
   save: (key, value) => {
     localStorage.setItem(key, JSON.stringify(value));
   },
   get: (key) => {
-    return JSON.parse(localStorage.getItem(key));
+    const columns = JSON.parse(localStorage.getItem(key))
+      .map((c) => {
+        // This is a need trick(gambi), because after deserialize
+        // from localstorage, the config return 'MultiCheckFilter'
+        return { ...c, filterRenderer: MultiCheckFilter };
+      });
+
+    return columns;
   },
   delete: key => localStorage.removeItem(key)
 };
