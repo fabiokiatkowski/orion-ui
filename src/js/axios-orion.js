20e20d3b784c472a9c7d3e82c75b65123e2030d5
Fabio Kiatkowski <xolla@xolla.com.br>
2018-01-18 14:49:46 -0200

    axio dev server

--- src/js/axios-orion.js
+++ src/js/axios-orion.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://192.168.1.120:8080/orion-server'
+  baseURL: 'http://192.168.0.193:8080'
 });
 
 export default instance;
