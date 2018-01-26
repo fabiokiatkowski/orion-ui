e0863019753e0fb6b6384c8b791f951fd34e6656
paulorsouza <paulor1809@gmail.com>
2018-01-26 09:04:08 -0200

    Configure axios to inject header authorization

--- src/js/axios-orion.js
+++ src/js/axios-orion.js
@@ -4,4 +4,13 @@ const instance = axios.create({
   baseURL: 'http://localhost:8080'
 });
 
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('orion.authToken');
+  const newConfig = config;
+  if (token) {
+    newConfig.headers.common.Authorization = token;
+  }
+  return newConfig;
+});
+
 export default instance;
