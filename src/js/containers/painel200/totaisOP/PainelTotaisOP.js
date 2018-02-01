bca87e48bba2eb2158574ccacbf2f4f7ef0db459
paulorsouza <paulor1809@gmail.com>
2018-02-01 11:33:17 -0200

    Refactor tabs on painel totais op

--- src/js/containers/painel200/totaisOP/PainelTotaisOP.js
+++ src/js/containers/painel200/totaisOP/PainelTotaisOP.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Tab } from 'react-bootstrap';
+import { Row, Col, Nav, NavItem, Tab, NavDropdown, MenuItem } from 'react-bootstrap';
 import ImageContainer from '../../../components/ImagesContainer';
 import InsumoNecessidade from '../../insumoNecessidade/InsumoNecessidade';
 import Sizeme from '../../../components/Sizeme';
@@ -11,54 +11,65 @@ const MainTabs = (props) => {
     renderInsumoOp = <InsumoNecessidade ordemProducao={props.ordemProducao} />;
   }
   return (
-    <Tabs activeKey={props.tabKey} onSelect={props.handleTabSelect} id="painel-totais-op-main">
-      <Tab eventKey={1} title="Observação">
-        <Observacao
-          ordemProducao={props.ordemProducao}
-          descEstagio={props.descEstagio}
-        />
-      </Tab>
-      <Tab eventKey={2} title="Insumos OP">
-        <div className="insumo-wrapper">
-          <Sizeme>
-            {renderInsumoOp}
-          </Sizeme>
-        </div>
-      </Tab>
-      <Tab eventKey={3} title="Estágio Paralelo">
-        Painel de Estágio Paralelo
-      </Tab>
-      <Tab eventKey={4} title="Grade de Corte">
-        Painel de Grade de Corte
-      </Tab>
-      <Tab eventKey={5} title="Observação">
-        Painel de Observação
-      </Tab>
-      <Tab eventKey={6} title="Onde Tem">
-        Painel de Onde tem
-      </Tab>
-      <Tab eventKey={7} title="Filhos">
-        Painel de Observação
-      </Tab>
-      <Tab eventKey={8} title="Log Uti">
-        Painel de Insumos
-      </Tab>
-      <Tab eventKey={9} title="Canc. OP">
-        Painel de Observação
-      </Tab>
-      <Tab eventKey={10} title="Altera Período">
-        Painel de Insumos
-      </Tab>
-    </Tabs>
+    <Tab.Container activeKey={props.tabKey} onSelect={props.handleTabSelect} id="painel-totais-op-main">
+      <Row className="clearfix">
+        <Col sm={12}>
+          <Nav bsStyle="tabs">
+            <NavDropdown eventKey="1" title="Observação" id="nav-dropdown-within-tab">
+              <MenuItem eventKey="1.1">Observação</MenuItem>
+              <MenuItem eventKey="1.2">Observação Systextil</MenuItem>
+              <MenuItem eventKey="1.3">Observação PeD</MenuItem>
+            </NavDropdown>
+            <NavItem eventKey="2">Insumos OP</NavItem>
+            <NavItem eventKey="3">Estágio Paralelo</NavItem>
+            <NavItem eventKey="4">Grade de Corte</NavItem>
+            <NavItem eventKey="5">Observação</NavItem>
+            <NavItem eventKey="6">Onde tem</NavItem>
+            <NavItem eventKey="7">Filhos</NavItem>
+            <NavItem eventKey="8">Log Uti</NavItem>
+            <NavItem eventKey="9">Canc. OP</NavItem>
+            <NavItem eventKey="10">Altera Período</NavItem>
+          </Nav>
+        </Col>
+        <Col sm={12}>
+          <Tab.Content animation>
+            <Tab.Pane eventKey="1.1">
+              <Observacao
+                ordemProducao={props.ordemProducao}
+                descEstagio={props.descEstagio}
+              />
+            </Tab.Pane>
+            <Tab.Pane eventKey="1.2">Tab 3.2 content</Tab.Pane>
+            <Tab.Pane eventKey="1.3">Tab 3.3 content</Tab.Pane>
+            <Tab.Pane eventKey="2">
+              <div className="insumo-wrapper">
+                <Sizeme>
+                  {renderInsumoOp}
+                </Sizeme>
+              </div>
+            </Tab.Pane>
+            <Tab.Pane eventKey="3">Painel de Estágio Paralelo</Tab.Pane>
+            <Tab.Pane eventKey="4">Painel de Estágio Paralelo</Tab.Pane>
+            <Tab.Pane eventKey="5">Painel de Estágio Paralelo</Tab.Pane>
+            <Tab.Pane eventKey="6">Painel de Estágio Paralelo</Tab.Pane>
+            <Tab.Pane eventKey="7">Painel de Estágio Paralelo</Tab.Pane>
+            <Tab.Pane eventKey="8">Painel de Estágio Paralelo</Tab.Pane>
+            <Tab.Pane eventKey="9">Painel de Estágio Paralelo</Tab.Pane>
+            <Tab.Pane eventKey="10">Painel de Estágio Paralelo</Tab.Pane>
+          </Tab.Content>
+        </Col>
+      </Row>
+    </Tab.Container>
   );
 };
 
 class PainelTotaisOP extends Component {
   state = {
-    tabMainKey: 1
+    tabMainKey: '1.1'
   }
 
   handleTabMainSelect = (tabMainKey) => {
+    console.log(tabMainKey);
     this.setState({ tabMainKey });
   }
 
