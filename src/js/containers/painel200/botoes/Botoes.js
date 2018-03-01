53de916e47e1375326dd1b2e55d66b9890a4d0bb
Fabio Kiatkowski <xolla@xolla.com.br>
2018-03-01 16:37:02 -0300

    Improve feedback ui for not implmented features

--- src/js/containers/painel200/botoes/Botoes.js
+++ src/js/containers/painel200/botoes/Botoes.js
@@ -1,94 +1,123 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import { Popover, OverlayTrigger, ButtonToolbar, Button } from 'react-bootstrap';
 import CancelarOP from '../totaisOP/cancelarOp/CancelarOp';
 
 const botoes = (props) => {
-  const handleNotImplemented = () => {
-    alert('Funcionalidade não disponível.');
-  };
   const classes = ['btn', 'btn-default', 'btn-margin-top', 'left'];
   if (props.disabled) {
     classes.push('disabled');
   }
+  const popoverHoverFocus = (
+    <Popover id="popover-positioned-bottom" title="Mensagem de aviso">
+      Funcionalidade não disponível.
+    </Popover>
+  );
   return (
     <div className="Botoes">
-      <button
-        className="btn btn-margin-top"
-        disabled={!props.onMarcarUTI || props.disabled}
-        onClick={props.onMarcarUTI}
-      >Marcar UTI
-      </button>
-      <button
-        disabled={!props.onDesmarcarUTI || props.disabled}
-        onClick={props.onDesmarcarUTI}
-        className="btn btn-margin-top left"
-      >Desmarcar UTI
-      </button>
-      <button
-        disabled={!props.onDesmarcarTodosUTI || props.disabled}
-        onClick={props.onDesmarcarTodosUTI}
-        className="btn btn-margin-top left"
-      >Desmarcar Todos UTI
-      </button>
-      <CancelarOP
-        ordemProducao={props.ordemProducao}
-        ordemPrincipal={props.ordemPrincipal}
-      />
-      <NavLink
-        className={classes.join(' ')}
-        target="_blank"
-        to={{ pathname: '/sus', search: `?ordemProducao=${props.ordemProducao}` }}
-      >SUS Almoxarifado
-      </NavLink>
-      <button
-        disabled={props.disabled}
-        onClick={handleNotImplemented}
-        className="btn btn-margin-top left"
-      >Ordem Serv. Manual
-      </button>
-      <NavLink
-        className={classes.join(' ')}
-        target="_blank"
-        to={{ pathname: '/visualizador', search: `?nivel=${props.nivel}&grupo=${props.grupo}` }}
-      >Visualizador
-      </NavLink>
-      {/* <button
-        disabled={props.disabled}
-        onClick={handleNotImplemented}
-        className="btn btn-margin-top left"
-      >SUS Almoxarifado
-      </button> */}
-      <button
-        disabled={props.disabled}
-        onClick={handleNotImplemented}
-        className="btn btn-margin-top left"
-      >Imprimir
-      </button>
-      <button
-        disabled={props.disabled}
-        onClick={handleNotImplemented}
-        className="btn btn-margin-top left"
-      >Prorrogar
-      </button>
-      <button
-        disabled={props.disabled}
-        onClick={handleNotImplemented}
-        className="btn btn-margin-top left"
-      >Consulta Avance
-      </button>
-      <button
-        disabled={props.disabled}
-        onClick={handleNotImplemented}
-        className="btn btn-margin-top left"
-      >Exportar Excel
-      </button>
-      <button
-        disabled={props.disabled}
-        onClick={handleNotImplemented}
-        className="btn btn-margin-top left"
-      >Estrutura
-      </button>
+      <ButtonToolbar>
+        <Button
+          bsClass="btn btn-margin-top"
+          disabled={!props.onMarcarUTI || props.disabled}
+          onClick={props.onMarcarUTI}
+        >Marcar UTI
+        </Button>
+        <Button
+          disabled={!props.onDesmarcarUTI || props.disabled}
+          onClick={props.onDesmarcarUTI}
+          bsClass="btn btn-margin-top left"
+        >Desmarcar UTI
+        </Button>
+        <Button
+          disabled={!props.onDesmarcarTodosUTI || props.disabled}
+          onClick={props.onDesmarcarTodosUTI}
+          bsClass="btn btn-margin-top left"
+        >Desmarcar Todos UTI
+        </Button>
+        <CancelarOP
+          ordemProducao={props.ordemProducao}
+          ordemPrincipal={props.ordemPrincipal}
+        />
+        <NavLink
+          className={classes.join(' ')}
+          target="_blank"
+          to={{ pathname: '/sus', search: `?ordemProducao=${props.ordemProducao}` }}
+        >SUS Almoxarifado
+        </NavLink>
+        <OverlayTrigger
+          trigger={!props.disabled ? ['focus'] : null}
+          placement="bottom"
+          overlay={popoverHoverFocus}
+        >
+          <Button
+            disabled={props.disabled}
+            bsClass="btn btn-margin-top left"
+          >Ordem Serv. Manual
+          </Button>
+        </OverlayTrigger>
+        <NavLink
+          className={classes.join(' ')}
+          target="_blank"
+          to={{ pathname: '/visualizador', search: `?nivel=${props.nivel}&grupo=${props.grupo}` }}
+        >Visualizador
+        </NavLink>
+        <OverlayTrigger
+          trigger={!props.disabled ? ['focus'] : null}
+          placement="bottom"
+          overlay={popoverHoverFocus}
+        >
+          <Button
+            disabled={props.disabled}
+            bsClass="btn btn-margin-top left"
+          >Imprimir
+          </Button>
+        </OverlayTrigger>
+        <OverlayTrigger
+          trigger={!props.disabled ? ['focus'] : null}
+          placement="bottom"
+          overlay={popoverHoverFocus}
+        >
+          <Button
+            disabled={props.disabled}
+            bsClass="btn btn-margin-top left"
+          >Prorrogar
+          </Button>
+        </OverlayTrigger>
+        <OverlayTrigger
+          trigger={!props.disabled ? ['focus'] : null}
+          placement="bottom"
+          overlay={popoverHoverFocus}
+        >
+          <Button
+            disabled={props.disabled}
+            bsClass="btn btn-margin-top left"
+          >Consultar Avance
+          </Button>
+        </OverlayTrigger>
+        <OverlayTrigger
+          trigger={!props.disabled ? ['focus'] : null}
+          placement="bottom"
+          overlay={popoverHoverFocus}
+        >
+          <Button
+            disabled={props.disabled}
+            bsClass="btn btn-margin-top left"
+          >Exportar Excel
+          </Button>
+        </OverlayTrigger>
+        <OverlayTrigger
+          trigger={!props.disabled ? ['focus'] : null}
+          placement="bottom"
+          overlay={popoverHoverFocus}
+        >
+          <Button
+            disabled={props.disabled}
+            bsClass="btn btn-margin-top left"
+          >Estrutura
+          </Button>
+        </OverlayTrigger>
+      </ButtonToolbar>
     </div>
   );
 };
